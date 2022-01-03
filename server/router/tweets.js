e6fca0375c96e819aa60126a6900f79bec60adcf
JOON HYUK LEE <60730765+anshqhsh@users.noreply.github.com>
2022-01-03 22:50:43 +0900

    validation- 유효성 검사 적용
    유효성 검사

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,17 +1,36 @@
 import express, { json } from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 const router = express.Router();
+
+// validation - 유효성 검사
+// sanitization
+
+// 참고
+// Contract Testing: Client-Server
+// Proto-base?
+
+const validateTweet = [
+  body('text')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('text should be at least 3 characters'),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username
 router.get('/', tweetController.getTweets); //값이 아닌 함수를 연결
 // GET /tweets/:id
 router.get('/:id', tweetController.getTweet);
+
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
+
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // DELETE /tweet/:id
 router.delete('/:id', tweetController.deleteTweet);
