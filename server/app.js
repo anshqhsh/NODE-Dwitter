67102138c39446330e942801a95d7661c1d2214e
JOON HYUK LEE <60730765+anshqhsh@users.noreply.github.com>
2022-01-16 15:44:32 +0900

    sequrlize 적용 완료

--- server/app.js
+++ server/app.js
@@ -7,9 +7,10 @@ import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
 import { config } from './config.js';
 import { initSocket } from './connection/socket.js';
-import { db, sequelize } from './db/database.js';
+import { sequelize } from './db/database.js';
 
 const app = express();
+
 // 미들웨어 : 거처가는 함수들 next를 통해 다음 미들웨어로 요청을 넘김
 app.use(express.json());
 // HTTP 헤더의 설정변경을 통해 위함한 웹취약점으로부터 서버를 보호
@@ -32,10 +33,7 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-db.getConnection().then(connection => console.log(connection));
-
-sequelize.sync().then(client => {
-  // console.log(client);
+sequelize.sync().then(() => {
   const server = app.listen(config.host.port);
   initSocket(server);
 }); //db 연결 해서 모델과 스키마가 테이블로 존재 하지 않는다면 테이블을 생성하는 함수
