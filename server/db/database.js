7041a217b2123ddddb4b87ca3f834cf9120ad4cf
JOON HYUK LEE <60730765+anshqhsh@users.noreply.github.com>
2022-01-18 20:09:06 +0900

    sequelize 적용 버전으로 마스터 브랜치 변경
    platform 배포중에 sequelize 배포로 문제 발생하여 mater로 브랜치를 변경해주고
    기준 적용점을 다시 바로 잡아줌

--- server/db/database.js
+++ server/db/database.js
@@ -1,12 +1,19 @@
 import mysql from 'mysql2';
 import { config } from '../config.js';
+import SQ from 'sequelize';
+
+const { host, user, database, password } = config.db;
+export const sequelize = new SQ.Sequelize(database, user, password, {
+  host,
+  dialect: 'mysql',
+});
 
 // mysql에 연결
 const pool = mysql.createPool({
-  host: config.db.host,
-  user: config.db.user,
-  database: config.db.database,
-  password: config.db.password,
+  host,
+  user,
+  database,
+  password,
 });
 
 // 프로미스로 출력
