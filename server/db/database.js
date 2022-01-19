93f0a178d877c16869ddacc357e6153b799cbf63
JOON HYUK LEE <60730765+anshqhsh@users.noreply.github.com>
2022-01-19 14:35:00 +0900

    Prep

--- server/db/database.js
+++ server/db/database.js
@@ -4,6 +4,13 @@ import SQ from 'sequelize';
 const { host, user, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
-  dialect: 'mysql',
+  port,
+  dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
